aaff1c832f49d3bac9eaef5fd1c67f33346f3a32
Karel <oricha@gmail.com>
2016-03-22 02:20:45 +0100

    Revert "moved game three to module and player tu module"
    This reverts commit e30cb873a597de317ce0ebf7211a15f5eeb6a104.

---
+++ src/test/java/org/takeaway/karel/GameControllerTests.java
@@ -0,0 +1,34 @@
+package org.takeaway.karel;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = GameControllerTests.class)
+@WebAppConfiguration
+public class GameControllerTests {
+
+	@Before
+	public void setUp() {
+	}
+
+	@Test
+	public void testMove() throws Exception {
+
+	}
+	
+	@Test
+	public void testGetPlayer() throws Exception {
+	}
+	
+	@Test
+	public void testStart() throws Exception {
+
+	}
+
+}
