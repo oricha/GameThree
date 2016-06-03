45e6291143dfba7cbfd3abca07e3be2a80bf5fe9
Karel <oricha@gmail.com>
2016-06-03 23:03:56 +0200

    Added Player

---
+++ Player/src/test/java/org/takeaway/karel/GameControllerTests.java
@@ -0,0 +1,39 @@
+package org.takeaway.karel;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(MockitoJUnitRunner.class)
+@SpringApplicationConfiguration(classes = GameControllerTests.class)
+@WebAppConfiguration
+public class GameControllerTests {
+
+	private static final Logger log = LoggerFactory.getLogger(GameControllerTests.class);
+
+	@Before
+	public void setUp() {
+
+	}
+
+	@Test
+	public void testMove() throws Exception {
+
+	}
+
+	@Test
+	public void testGetPlayer() throws Exception {
+		
+	}
+
+	@Test
+	public void testStart() throws Exception {
+
+	}
+
+}
